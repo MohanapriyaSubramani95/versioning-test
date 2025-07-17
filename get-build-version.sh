e42e90b77d2fd7a04d5ecf364a93f8735261ea8d
Mohanapriya <mohana.priya@lifelabkids.org>
2025-07-17 16:24:54 +0530

    build changes

--- get-build-version.sh
+++ get-build-version.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 
-current_environment=$1
-component_name=$2
-version_postfix=$3
+current_environment=$1       # dev / uat / prod
+component_name=$2            # frontend, backend, etc.
+version_postfix=$3           # git sha or build id
 
 git fetch --tags
 
-last_tag_pattern="$component_name-*"
+# Default last tag pattern
+last_tag_pattern="$component_name-$current_environment-*"
 
-is_patch=false
+# Fetch last tag
+matching_tags=$(git tag --list "$last_tag_pattern" | sort -Vr)
+last_tag=$(echo "$matching_tags" | head -n 1)
 
-if [ "$current_environment" == "uat" ] || [ "$current_environment" == "prod" ]; then
-  current_commit=$(git rev-parse HEAD)
-
-  # Detect if commit message includes 'merge dev' (case-insensitive)
-  git log -1 --pretty=%B | grep -iq "merge.*dev"
-  if [ $? -eq 0 ]; then
-    is_patch=false
-  else
-    is_patch=true
-  fi
-
-  last_tag_pattern="$component_name-$current_environment-*"
-fi
-
-matching_tags=$(git tag --list "$last_tag_pattern" | sort -r)
-tag_count=$(echo "$matching_tags" | wc -l)
-
-if [ "$tag_count" -eq 0 ]; then
-  last_tag=""
-else
-  last_tag=$(echo "$matching_tags" | head -n 1)
-fi
-
-if [[ $last_tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
+# Parse version
+if [[ "$last_tag" =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
   major=${BASH_REMATCH[1]}
   minor=${BASH_REMATCH[2]}
   patch=${BASH_REMATCH[3]}
@@ -43,15 +24,33 @@ else
   patch=0
 fi
 
-if [ "$is_patch" == "true" ]; then
+# Get last commit message
+commit_message=$(git log -1 --pretty=%B)
+
+# Determine bump type
+if [ "$current_environment" == "dev" ]; then
   patch=$((patch + 1))
-else
-  minor=$((minor + 1))
-  patch=0
+
+elif [ "$current_environment" == "uat" ]; then
+  if echo "$commit_message" | grep -iq "merge.*dev"; then
+    minor=$((minor + 1))
+    patch=0
+  else
+    patch=$((patch + 1))
+  fi
+
+elif [ "$current_environment" == "prod" ]; then
+  if echo "$commit_message" | grep -iq "merge.*uat"; then
+    major=$((major + 1))
+    minor=0
+    patch=0
+  else
+    patch=$((patch + 1))
+  fi
 fi
 
 new_version="$major.$minor.$patch"
 new_tag="$component_name-$current_environment-$new_version-$version_postfix"
 
-# IMPORTANT: Output ONLY this line for GitHub Actions to capture as output
+# Output for GitHub Actions
 echo "version_tag=$new_tag"
