355ac52af925bc68e8a70a77774fafc8c5269ac9
Kavinkumar S <kavingowthaman@gmail.com>
2025-07-25 07:52:47 +0530

    build changes

--- get-build-version.sh
+++ get-build-version.sh
@@ -1,54 +1,55 @@
 #!/bin/bash
 
-current_env=$1         # dev, uat, or prod
-component=$2           # e.g. frontend
-version_postfix=$3     # e.g. commit hash
+current_environment=$1
+component_name=$2
+version_postfix=$3
 
 git fetch --tags
 
-# Helper to get latest tag for an environment
-get_latest_tag() {
-  local env=$1
-  git tag --list "$component-$env-*" | sort -V | tail -n 1
-}
+last_tag_pattern="$component_name-*"
 
-# Parse semver from tag, or default to 0.0.0
-parse_version() {
-  local tag=$1
-  if [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
-    echo "${BASH_REMATCH[1]} ${BASH_REMATCH[2]} ${BASH_REMATCH[3]}"
-  else
-    echo "0 0 0"
+is_patch=false
+
+if [ "$current_environment" == "uat" ] || [ "$current_environment" == "prod" ]; then
+  current_commit=$(git rev-parse HEAD)
+  number_of_branches_with_commit=$(git branch -a --contains "$current_commit" | grep -c "remotes/origin/[dev|uat|prod]")
+
+  if [ "$number_of_branches_with_commit" == "1" ]; then
+    is_patch=true
   fi
-}
+fi
 
-if [ "$current_env" == "dev" ]; then
-  # For dev, bump patch from latest prod tag
-  latest_tag=$(get_latest_tag "prod")
-  read major minor patch <<< $(parse_version "$latest_tag")
-  patch=$((patch + 1))
+if [ $is_patch == true ]; then
+  last_tag_pattern="$component_name-$current_environment-*"
+fi
 
-elif [ "$current_env" == "uat" ]; then
-  # For uat, bump minor from latest dev tag, keep major from dev
-  latest_tag=$(get_latest_tag "dev")
-  read major minor patch <<< $(parse_version "$latest_tag")
-  minor=$((minor + 1))
-  patch=0
+number_of_existing_tags=$(git tag --list "$last_tag_pattern" | wc -l)
 
-elif [ "$current_env" == "prod" ]; then
-  # For prod, bump major from latest uat tag, reset minor and patch
-  latest_tag=$(get_latest_tag "uat")
-  read major minor patch <<< $(parse_version "$latest_tag")
+if [ "$number_of_existing_tags" != "0" ]; then
+  last_tag=$(git describe --match "$last_tag_pattern" --abbrev=0 --tags "$(git rev-list --tags --max-count=1)")
+fi
+
+major=0
+minor=0
+patch=0
+
+if [[ $last_tag ]]; then
+  version_number=$(echo "$last_tag" | cut -d- -f3)
+
+  major=$(echo "$version_number" | cut -d. -f1)
+  minor=$(echo "$version_number" | cut -d. -f2)
+  patch=$(echo "$version_number" | cut -d. -f3)
+fi
+
+if [ "$current_environment" == "prod" ] && [ $is_patch == false ]; then
   major=$((major + 1))
   minor=0
   patch=0
-
+elif [ "$current_environment" == "uat" ] && [ $is_patch == false ]; then
+  minor=$((minor + 1))
+  patch=0
 else
-  echo "Unsupported environment: $current_env"
-  exit 1
+  patch=$((patch + 1))
 fi
 
-new_version="${major}.${minor}.${patch}"
-new_tag="${component}-${current_env}-${new_version}-${version_postfix}"
-
-echo "$new_tag"
+echo "$component_name-$current_environment-$major.$minor.$patch-$version_postfix"
